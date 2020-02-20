491d617924ec626b1b5f64d683d0de9e1b826fa4
fredsonchaves07 <fredsonchaves07@gmail.com>
2020-02-20 13:30:46 -0300

    Modificação na estrutura de view e aplicação de layout.njk padrão em todas as páginas

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const nunjucks = require('nunjucks')
+const dados = require('./data')
 
 const server = express()
 
@@ -7,10 +8,22 @@ server.use(express.static('public'))
 
 server.set('view engine', 'njk')
 
-server.listen(5000, function(){
-    console.log('Em Execução..')
+nunjucks.configure('views', {
+    express: server
 })
 
 server.get('/', function(req, res){
     res.render('index')
+})
+
+server.get('/receitas', function(req, res){
+    res.render('receitas')
+})
+
+server.get('/sobre', function(req, res){
+    res.render('sobre')
+})
+
+server.listen(5000, function(){
+    console.log('Em Execução..')
 })
