edf6affe9fa93bd589d27b3d61f577ffde5e4dab
fredson chaves <fredsonchaves07@gmail.com>
2020-05-21 13:23:31 -0300

    feature: criaçao de controllers e mudança na estrutura das rotas da pagina admin

---
+++ controllers/receitas.js
@@ -0,0 +1,37 @@
+
+
+//index
+exports.index = function(req, res){
+
+}
+
+//show
+exports.show = function(req, res){
+
+}
+
+//create
+exports.create = function(req, res){
+
+}
+
+//edit
+exports.edit = function(req, res){
+
+}
+
+//post
+exports.post = function(req, res){
+
+
+}
+
+//put
+exports.put = function(req, res){
+
+}
+
+//delete
+exports.delete = function(req, res){
+
+}
--- router.js
+++ router.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const routes = express.Router()
 const dados = require('./data')
+const receitas = require('./controllers/receitas')
 
 routes.get('/', function(req, res){
     return res.render('index', {dados: dados})
@@ -20,5 +21,15 @@ routes.get('/receitas/:id', function(req, res){
 routes.get('/sobre', function(req, res){
     return res.render('sobre')
 })
+
+//Rotas para página administrativa
+routes.get('/admin/receitas', receitas.index)
+routes.get('/admin/receitas/create', receitas.create)
+routes.get('/admin/receitas/:id', receitas.show)
+routes.get('/admin/receitas/:id/edit', receitas.edit)
+
+routes.post('/admin/receitas', receitas.post)
+routes.put('/admin/receitas', receitas.put)
+routes.delete('/admin/receitas', receitas.delete)
 
 module.exports = routes
